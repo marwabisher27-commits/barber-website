8273a8fb286ee4a6d9f0a2b07bfcf6f70b68d7e2
marwabisher27-commits <marwabisher27@gmail.com>
2026-05-07 23:44:22 +0300

    fix package first cut count

--- admin.js
+++ admin.js
@@ -77,7 +77,7 @@ function showFullWeek() {
                         <p>${appointment.phone}</p>
                         <p>${appointment.service}</p>
                         <p>${appointment.payment === "cash" ? "מזומן במספרה" : "אשראי"}</p>
-                           ${appointment.package ? `<p>חבילה: ${appointment.package.type} | נותרו: ${appointment.package.remainingCuts}</p>` : ""}
+                           ${appointment.package ? `<p>חבילה: ${appointment.package.type} | נותרו: ${appointment.package.remainingCuts-1}</p>` : ""}
                            ${appointment.usedPackage ? `<p>שולם מחבילה | נותרו: ${appointment.usedPackage.remainingAfter}</p>` : ""}
                         <button onclick="adminCancelBooking('${appointment.id}', '${appointment.day}', '${appointment.time}')">
                             ביטול תור
